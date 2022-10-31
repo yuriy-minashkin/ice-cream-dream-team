aaa06a75f259c5b1a3ffa8c76276f63f3216e442
PatrickJericho <patrick.jericho368@gmail.com>
2022-10-31 16:20:31 +0200

    1.5

--- src/js/burger_menu.js
+++ src/js/burger_menu.js
@@ -3,11 +3,16 @@
 (() => {
     const menuBtnRef = document.querySelector("[mobile-menu-button]");
     const mobileMenuRef = document.querySelector("[mobile-menu]");
+    const mobileLink = document.querySelectorAll(".mob-nav__link");
     menuBtnRef.addEventListener("click", () => {
         const expanded =
             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
         menuBtnRef.classList.toggle("is-open");
         menuBtnRef.setAttribute("aria-expanded", !expanded);
         mobileMenuRef.classList.toggle("is-open");
+    });
+    mobileLink.addEventListener("click", () => {        
+        mobileMenuRef.classList.remove("is-open"); 
+        menuBtnRef.classList.remove("is-open");
     });
 })();
